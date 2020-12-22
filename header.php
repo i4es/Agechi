52f76a44036058f692cf374737b972a8ce5d009a
i4es <dss4game@gmail.com>
2020-12-22 20:22:45 +0200

    Add custom logo support and header thml

--- header.php
+++ header.php
@@ -11,40 +11,50 @@
 ?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+	<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+<?php endif; ?>
 
-	<header class="site-header" role="banner">
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-			<div class="title-bar-left">
-				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</span>
-			</div>
-		</div>
+<header class="ag-header">
+	<div class="grid-container">
+		<div class="grid-x grid-margin-x">
 
-		<nav class="site-navigation top-bar" role="navigation" id="<?php foundationpress_mobile_menu_id(); ?>">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			<div class="ag-header__inner">
+				<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
+					<div class="title-bar-left">
+						<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+					</div>
+				</div>
+
+				<div class="top-bar-left">
+					<div class="ag-logo">
+						<div class="ag-logo__circle"></div>
+						<?php show_custom_logo(); ?>
+					</div>
+				</div>
+
+				<nav class="site-navigation top-bar" role="navigation" id="<?php foundationpress_mobile_menu_id(); ?>">
+					<div class="top-bar-right">
+						<?php foundationpress_top_bar_r(); ?>
+
+						<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+							<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+						<?php endif; ?>
+					</div>
+				</nav>
+
+				<div class="site-in-touch">
+					<a class="site-in-touch__link" href="#">Get in touch</a>
 				</div>
 			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
 
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
-		</nav>
-
-	</header>
+		</div>
+	</div>
+</header>
--- library/theme-support.php
+++ library/theme-support.php
@@ -52,3 +52,21 @@ if ( ! function_exists( 'foundationpress_theme_support' ) ) :
 
 	add_action( 'after_setup_theme', 'foundationpress_theme_support' );
 endif;
+
+// CUSTOM LOGO
+add_theme_support( 'custom-logo', array(
+	'height'      => '150',
+	'flex-height' => true,
+	'flex-width'  => true,
+) );
+function show_custom_logo( $size = 'medium' ) {
+	if ( $custom_logo_id = get_theme_mod( 'custom_logo' ) ) {
+		$attachment_array = wp_get_attachment_image_src( $custom_logo_id, $size );
+		$logo_url         = $attachment_array[0];
+	} else {
+		$logo_url = get_stylesheet_directory_uri() . '/images/custom-logo.png';
+	}
+	$logo_image = '<img src="' . $logo_url . '" class="custom-logo" itemprop="siteLogo" alt="' . get_bloginfo( 'name' ) . '">';
+	$html       = sprintf( '<a href="%1$s" class="custom-logo-link" rel="home" title="%2$s" itemscope>%3$s</a>', esc_url( home_url( '/' ) ), get_bloginfo( 'name' ), $logo_image );
+	echo apply_filters( 'get_custom_logo', $html );
+}
